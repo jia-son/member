efc9af471e29020a27fd0f43d030549ce81231e7
jia-son <116625758+jia-son@users.noreply.github.com>
2023-04-17 15:28:42 +0900

    Add MemberService.java, MemberDTO.java

---
+++ src/main/java/com/codingrecipe/member/dto/MemberDTO.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.member.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class MemberDTO {
+}
