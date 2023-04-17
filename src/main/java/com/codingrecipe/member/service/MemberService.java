786c24ee780dcd4e5562efee85c3dd8fd6a21a97
jia-son <116625758+jia-son@users.noreply.github.com>
2023-04-17 21:56:15 +0900

    add MemberService : login메서드 추가

--- src/main/java/com/codingrecipe/member/service/MemberService.java
+++ src/main/java/com/codingrecipe/member/service/MemberService.java
@@ -6,6 +6,9 @@ import com.codingrecipe.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Member;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -17,5 +20,24 @@ public class MemberService {
 
         MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
         memberRepository.save(memberEntity); //save는 JPA가 제공해주는 메서드이다.
+    }
+
+    public MemberDTO login(MemberDTO memberDTO) {
+        /*
+        1. 회원이 입력한 이메일로 DB 조회
+        2. DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치하는지 판단
+        * */
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+        if(byMemberEmail.isPresent()) {
+            MemberEntity memberEntity = byMemberEmail.get();
+            if(memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+                MemberDTO dto = MemberDTO.toMemberDTO(memberEntity);
+                return dto;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 }
