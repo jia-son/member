df1bfac40c3d337bb97d277bfe8084b88224d92b
jia-son <116625758+jia-son@users.noreply.github.com>
2023-04-17 21:21:49 +0900

    add MemberService, MemberEntity : 메서드 추가

--- src/main/java/com/codingrecipe/member/entity/MemberEntity.java
+++ src/main/java/com/codingrecipe/member/entity/MemberEntity.java
@@ -1,5 +1,6 @@
 package com.codingrecipe.member.entity;
 
+import com.codingrecipe.member.dto.MemberDTO;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,4 +23,12 @@ public class MemberEntity {
 
     @Column
     private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
 }
--- src/main/java/com/codingrecipe/member/service/MemberService.java
+++ src/main/java/com/codingrecipe/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.codingrecipe.member.service;
 
 import com.codingrecipe.member.dto.MemberDTO;
+import com.codingrecipe.member.entity.MemberEntity;
 import com.codingrecipe.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,11 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     private final MemberRepository memberRepository;
     public void save(MemberDTO memberDTO) {
+        //repository의 save메서드 호출 (조건: entity객체를 넘겨줘야 함)
+        //1. dto -> entity 변환
+        //2. repository의 save메서드 호출
+
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+        memberRepository.save(memberEntity); //save는 JPA가 제공해주는 메서드이다.
     }
 }
