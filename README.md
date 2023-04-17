7002d8f2041f63a00c2188fa1cf39ee194333ca5
jia-son <116625758+jia-son@users.noreply.github.com>
2023-04-17 14:41:28 +0900

    Add MemberController.java, save.html
    + Modify README.md

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ https://www.youtube.com/watch?v=RhM1bQ76Tv0&list=PLV9zd3otBRt5ANIjawvd-el3QU594w
 
 위 링크 참고하여 진행중
 
+* RESTFul한 것이 아니라 html화면 자체를 반환하는 방식으로 진행되고 있다.
 -----
 커밋시 이름 규칙?
 1. Add > (File/Class 명) > Code/Content 등
--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-    @GetMapping("/")
-    public String index() {
-        return "index";
+public class MemberController {
+    //회원가입 페이지 출력 요청
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
     }
 }
