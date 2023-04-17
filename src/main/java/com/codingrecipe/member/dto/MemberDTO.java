06160258ddae232c8073b6fd6ec815018386464f
jia-son <116625758+jia-son@users.noreply.github.com>
2023-04-17 16:13:13 +0900

    Add code > application.yml, MemberService.java, MemberDTO.java, MemberController.java

--- src/main/java/com/codingrecipe/member/dto/MemberDTO.java
+++ src/main/java/com/codingrecipe/member/dto/MemberDTO.java
@@ -1,13 +1,15 @@
 package com.codingrecipe.member.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class MemberDTO {
+    private Long id;
+    private String memberEmail;
+    private String memberPassword;
+    private String memberName;
 }
--- src/main/java/com/codingrecipe/member/service/MemberService.java
+++ src/main/java/com/codingrecipe/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.codingrecipe.member.service;
 
+import com.codingrecipe.member.dto.MemberDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
-
+    private final MemberReporitory memberReporitory;
+    public void save(MemberDTO memberDTO) {
+    }
 }
