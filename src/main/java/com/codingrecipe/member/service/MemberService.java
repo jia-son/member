44c56ca85e2b75609693ec27ea6489b1d620f111
jia-son <116625758+jia-son@users.noreply.github.com>
2023-04-18 10:35:06 +0900

    add MemberService : 회원목록을 통해 db의 정보를 뽑아오기 위한 코드 추가

--- src/main/java/com/codingrecipe/member/service/MemberService.java
+++ src/main/java/com/codingrecipe/member/service/MemberService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -39,5 +41,14 @@ public class MemberService {
         } else {
             return null;
         }
+    }
+
+    public List<MemberDTO> findAll() {
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        for(MemberEntity memberEntity : memberEntityList) {
+            memberDTOList.add(MemberDTO.toMemberDTO(memberEntity));
+        }
+        return memberDTOList;
     }
 }
